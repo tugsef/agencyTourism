38dd371cd5efd304db2f5406427e84e9c63b7ab5
Sefa Demirtaş <sefa.demirtas91@gmail.com>
2023-05-14 02:53:37 +0300

    start Tourism Agency Application

---
+++ src/main/java/com/example/tourismAgency/AgencyTourismApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourismAgency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyTourismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyTourismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agencyTourist/AgencyTouristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agencyTourist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyTouristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
