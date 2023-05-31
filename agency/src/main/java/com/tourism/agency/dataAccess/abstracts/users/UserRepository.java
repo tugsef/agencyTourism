9233ffb8d3c118ddc5336913e6d287e0e966fd09
Sefa Demirtaş <sefa.demirtas91@gmail.com>
2023-05-31 12:04:38 +0300

    update

--- agency/src/main/java/com/tourism/agency/dataAccess/abstracts/users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tourism.agency.dataAccess.abstracts.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tourism.agency.entities.user.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-	}
